bb695f3541fee32a1fb283850c8d53332f6afae5
emin4ph <89451840+emin4ph@users.noreply.github.com>
2021-08-24 14:24:15 +0400

    Template

---
+++ js/fashion.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      // init: false,
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+      }
+    });
